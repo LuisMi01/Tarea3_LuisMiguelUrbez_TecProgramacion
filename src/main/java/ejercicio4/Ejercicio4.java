143648cebb8de738a009360d68fdcf4e0cff9f24
LuisMi01 <lmurbezv@gmail.com>
2023-02-16 11:58:50 +0100

    terminado ejercicio 4

--- src/main/java/ejercicio4/Ejercicio4.java
+++ src/main/java/ejercicio4/Ejercicio4.java
@@ -1,6 +1,8 @@
 package ejercicio4;
 import java.util.Scanner;
+
 public class Ejercicio4 {
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("¿Cual es tu tipo de carne? (1 - vacuno, 2 - cordero)");
@@ -10,36 +12,44 @@ public class Ejercicio4 {
         System.out.println("¿Cual es el grado de cocinado? (1 - casi cruda, 2 - al punto, 3 - Bien hecho)");
         int cocinado = teclado.nextInt();
 
-        if (carne == 1){
-
-        }
-
-        if (carne == 2){}
-
-
-        public void calcularTiempos(){
+        if (carne == 1) {
             double tiempoBase = 10;
             int misgramos = gramos;
             int gramosBase = 500;
             double tiempo;
-            if (cocinado == 1){
-                tiempo = ((misgramos*tiempoBase) / gramosBase);
+            if (cocinado == 1) {
+                tiempo = ((misgramos * tiempoBase) / gramosBase);
                 System.out.println("El tiempo para la carne casi cruda es de " + tiempo + " minutos");
             } else if (cocinado == 2) {
-                double tiempo2 = ((misgramos*tiempoBase) / gramosBase) * 1.66;
+                double tiempo2 = ((misgramos * tiempoBase) / gramosBase) * 1.66;
                 System.out.println("El tiempo para la carne al punto es de " + tiempo2 + " minutos");
-            }else if (cocinado == 2) {
-                double tiempo3 = (((misgramos*tiempoBase) / gramosBase) * 1.66)*1.66;
+            } else if (cocinado == 3) {
+                double tiempo3 = (((misgramos * tiempoBase) / gramosBase) * 1.66) * 1.66;
                 System.out.println("El tiempo para la carne bien hecha es de " + tiempo3 + " minutos");
             }
+        }
 
+        if (carne == 2) {
+            double tiempoBase = 15;
+            int misgramos = gramos;
+            int gramosBase = 400;
+            double tiempo;
+            if (cocinado == 1) {
+                tiempo = ((misgramos * tiempoBase) / gramosBase);
+                System.out.println("El tiempo para la carne casi cruda es de " + tiempo + " minutos");
+            } else if (cocinado == 2) {
+                double tiempo2 = ((misgramos * tiempoBase) / gramosBase) * 1.66;
+                System.out.println("El tiempo para la carne al punto es de " + tiempo2 + " minutos");
+            } else if (cocinado == 3) {
+                double tiempo3 = (((misgramos * tiempoBase) / gramosBase) * 1.66) * 1.66;
+                System.out.println("El tiempo para la carne bien hecha es de " + tiempo3 + " minutos");
             }
-
-
-
-
-
         }
 
     }
+
+
+
 }
+
+
