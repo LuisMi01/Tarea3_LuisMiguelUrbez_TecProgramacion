42365ec32764e48cbb56ed1d7343fbc6d722cf6d
LuisMi01 <lmurbezv@gmail.com>
2023-02-16 14:45:50 +0100

    Terminando ejercicio 5

--- src/main/java/ejercicio5/ejercicio5.java
+++ src/main/java/ejercicio5/ejercicio5.java
@@ -1,5 +1,6 @@
 package ejercicio5;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ejercicio5 {
@@ -55,9 +56,55 @@ public class ejercicio5 {
     public static void calculoDeducciones(double salarioBruto, int hijos){
         System.out.println("Calculo de contribuciones:\n");
 
-        
-    }
+        System.out.println("Contribución para el pago de la deuda social y contingencias comunes imponible:");
+        double deduccion1 = salarioBruto * 3.49;
+        System.out.println(salarioBruto + " * 3.49 % = " + deduccion1 + " €\n");
 
+        System.out.println("Contribución de contingencias comunes no imponible:");
+        double deduccion2 = salarioBruto * 6.15;
+        System.out.println(salarioBruto + " * 6.15 % = " + deduccion2 + " €\n");
+
+        System.out.println("Seguro médico privado:");
+        double deduccion3 = salarioBruto * 0.95;
+        System.out.println(salarioBruto + " * 0.95 % = " + deduccion3 + " €\n");
+
+        System.out.println("Fondo de pensiones");
+        double deduccion4 = salarioBruto * 8.44;
+        System.out.println(salarioBruto + " * 8.44 % = " + deduccion4 + " €\n");
+
+        System.out.println("Seguro de desempleo");
+        double deduccion5 = salarioBruto * 3.05;
+        System.out.println(salarioBruto + " * 3.05 % = " + deduccion5 + " €\n");
+
+        System.out.println("Pensión complementaria (Entidad privada)");
+        double deduccion6 = salarioBruto * 3.81;
+        System.out.println(salarioBruto + " * 3.81 % = " + deduccion6 + " €\n");
+
+        System.out.println("Contribución de jubilación anticipada");
+        double deduccion7 = salarioBruto * 1.02;
+        System.out.println(salarioBruto + " * 1.02 % = " + deduccion7 + " €\n");
+
+        double deduccionTotal = deduccion1 + deduccion2 + deduccion3 + deduccion4 + deduccion5 + deduccion6 + deduccion7;
+        System.out.println("Deducciones totales del empleado: " + deduccionTotal + " €\n");
+
+        System.out.println("Salario neto: " + (salarioBruto - deduccionTotal) + " €\n");
+        calculoHijos(hijos);
+
+
+
+    }
+    public static void calculoHijos(int hijos){
+        if (hijos == 1){
+            int deduccionHijos = 20;
+            System.out.println("Prima familiar: " + deduccionHijos + " €\n");
+        } else if (hijos == 2){
+            int deduccionHijos = 50;
+            System.out.println("Prima familiar: " + deduccionHijos + " €\n");
+        } else if (hijos >2) {
+            int deduccionHijos = 70 + (hijos - 2) * 20;
+            System.out.println("Prima familiar: " + deduccionHijos + " €\n");
+        }
+    }
 
 
 
